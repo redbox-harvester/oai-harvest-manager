703457aabc06ee78c0a49033d27d78179c42d236
mattRedBox <matt@redboxresearchdata.com.au>
2017-02-25 08:38:34 +1000

    simplified logger. added total for workers so parent process can order itself around known running threads.

--- src/main/java/nl/mpi/oai/harvester/control/Main.java
+++ src/main/java/nl/mpi/oai/harvester/control/Main.java
@@ -18,6 +18,7 @@
 
 package nl.mpi.oai.harvester.control;
 
+import net.sf.saxon.expr.PJConverter;
 import nl.mpi.oai.harvester.Provider;
 import nl.mpi.oai.harvester.cycle.Cycle;
 import nl.mpi.oai.harvester.cycle.CycleFactory;
@@ -29,6 +30,10 @@ import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.xpath.XPathExpressionException;
 import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 
 /**
@@ -40,88 +45,89 @@ public class Main {
     private static final String sep = System.getProperty("file.separator");
     private static final Logger logger = LogManager.getLogger(Main.class);
 
-    /** Object containing entries from configuration file. */
+    /**
+     * Object containing entries from configuration file.
+     */
     public static Configuration config;
 
     private static void runHarvesting(Configuration config) {
-	config.log();
+        config.log();
 
-	// Start a new worker thread for each provider. The Worker class
-	// is responsible for honouring the configured limit of
-	// concurrent worker threads.
-	Worker.setConcurrentLimit(config.getMaxJobs());
-	// create a CycleFactory
-	CycleFactory factory = new CycleFactory();
-	// get a cycle based on the overview file
-	File OverviewFile = new File (config.getOverviewFile());
-	Cycle cycle = factory.createCycle(OverviewFile);
+        // Start a new worker thread for each provider. The Worker class
+        // is responsible for honouring the configured limit of
+        // concurrent worker threads.
+        Worker.setConcurrentLimit(config.getMaxJobs());
+        // create a CycleFactory
+        CycleFactory factory = new CycleFactory();
+        // get a cycle based on the overview file
+        File OverviewFile = new File(config.getOverviewFile());
+        Cycle cycle = factory.createCycle(OverviewFile);
 
-	for (Provider provider : config.getProviders()) {
-
-		// create a new working, passing one and the same for each cycle
-	    Worker worker = new Worker(
-				provider, config.getActionSequences(), cycle);
-
-	    worker.startWorker();
-
-	}
+        if (Worker.getTotal() != 0) {
+            throw new IllegalStateException("There is a previous harvest which still has workers or the worker count has not reset correctly. Aborting.");
+        }
+        for (Provider provider : config.getProviders()) {
+            // create a new working, passing one and the same for each cycle
+            Worker worker = new Worker(
+                    provider, config.getActionSequences(), cycle);
+            worker.startWorker();
+        }
     }
 
+
     public static void main(String[] args) {
-        
         logger.info("Welcome to the main OAI Harvest Manager!");
-        
-	String configFile = null;
+        String configFile = null;
 
-	// Select Saxon XSLT/XPath implementation (necessary in case there
-    // are other XSLT/XPath libraries in classpath).
-    System.setProperty("javax.xml.transform.TransformerFactory",    
-        "net.sf.saxon.TransformerFactoryImpl");
-    System.setProperty("javax.xml.xpath.XPathFactory",
-        "net.sf.saxon.xpath.XPathFactoryImpl");
+        // Select Saxon XSLT/XPath implementation (necessary in case there
+        // are other XSLT/XPath libraries in classpath).
+        System.setProperty("javax.xml.transform.TransformerFactory",
+                "net.sf.saxon.TransformerFactoryImpl");
+        System.setProperty("javax.xml.xpath.XPathFactory",
+                "net.sf.saxon.xpath.XPathFactoryImpl");
 
-	// If the "config" parameter is specified, take it as the
-	// configuration file name.
-	for (String arg : args) {
-	    if (arg.startsWith("config=")) {
-		configFile = arg.substring(7);
-	    } else if (!arg.contains("=")) {
-		configFile = arg;
-	    }
-	}
+        // If the "config" parameter is specified, take it as the
+        // configuration file name.
+        for (String arg : args) {
+            if (arg.startsWith("config=")) {
+                configFile = arg.substring(7);
+            } else if (!arg.contains("=")) {
+                configFile = arg;
+            }
+        }
 
-	// If a configuration file name hasn't been specified in the
-	// arguments, use "resources/config.xml" by default.
-	if (configFile == null) {
-	    configFile = "resources" + sep + "config.xml";
-	}
+        // If a configuration file name hasn't been specified in the
+        // arguments, use "resources/config.xml" by default.
+        if (configFile == null) {
+            configFile = "resources" + sep + "config.xml";
+        }
 
-	// Process options given on the command line (if any), then read the
-	// configuration file. 
-	config = new Configuration();
-	for (String arg : args) {
-	    if (arg.indexOf('=') > -1) {
-		String[] tmp=arg.split("=");
-		if (tmp.length == 1) {
-		    config.setOption(tmp[0], null);
-		} else if (tmp.length >= 2) {
-		    config.setOption(tmp[0], tmp[1]);
-		}
-	    }
-	}
-	try {
-	    config.readConfig(configFile);
-	} catch (ParserConfigurationException | SAXException 
-		| XPathExpressionException | IOException ex) {
-	    logger.error("Unable to read configuration file", ex);
-	    return;
-	}
+        // Process options given on the command line (if any), then read the
+        // configuration file.
+        config = new Configuration();
+        for (String arg : args) {
+            if (arg.indexOf('=') > -1) {
+                String[] tmp = arg.split("=");
+                if (tmp.length == 1) {
+                    config.setOption(tmp[0], null);
+                } else if (tmp.length >= 2) {
+                    config.setOption(tmp[0], tmp[1]);
+                }
+            }
+        }
+        try {
+            config.readConfig(configFile);
+        } catch (ParserConfigurationException | SAXException
+                | XPathExpressionException | IOException ex) {
+            logger.error("Unable to read configuration file", ex);
+            return;
+        }
 
-	// Ensure the timeout setting is honored.
-	config.applyTimeoutSetting();
+        // Ensure the timeout setting is honored.
+        config.applyTimeoutSetting();
 
-	runHarvesting(config);
-        
+        runHarvesting(config);
+
         logger.info("Goodbye from the main OAI Harvest Manager!");
 
     }
--- src/main/java/nl/mpi/oai/harvester/control/Worker.java
+++ src/main/java/nl/mpi/oai/harvester/control/Worker.java
@@ -32,6 +32,7 @@ import org.apache.logging.log4j.ThreadContext;
 
 import java.util.List;
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * This class represents a single processing thread in the harvesting actions
@@ -42,17 +43,25 @@ import java.util.concurrent.Semaphore;
  *
  * @author Lari Lampen (MPI-PL), extensions by Kees Jan van de Looij (MPI-PL).
  */
-class Worker implements Runnable {
-    
+public class Worker implements Runnable {
+
     private static final Logger logger = LogManager.getLogger(Worker.class);
-    
-    /** A standard semaphore is used to track the number of running threads. */
+
+    /**
+     * A standard semaphore is used to track the number of running threads.
+     */
     private static Semaphore semaphore;
 
-    /** The provider this worker deals with. */
+    private static AtomicInteger total = new AtomicInteger();
+
+    /**
+     * The provider this worker deals with.
+     */
     private final Provider provider;
 
-    /** List of actionSequences to be applied to the harvested metadata. */
+    /**
+     * List of actionSequences to be applied to the harvested metadata.
+     */
     private final List<ActionSequence> actionSequences;
 
     /* Harvesting scenario to be applied. ListIdentifiers: first, based on
@@ -67,51 +76,58 @@ class Worker implements Runnable {
 
     /**
      * Set the maximum number of concurrent worker threads.
-     * 
+     *
      * @param num number of running threads that may not be exceeded
      */
     public static void setConcurrentLimit(int num) {
-	semaphore = new Semaphore(num);
+        semaphore = new Semaphore(num);
     }
+
+    public static int getTotal() {
+        return total.get();
+    }
+
 
     /**
      * Associate a provider and action actionSequences with a scenario
      *
-     * @param provider OAI-PMH provider that this thread will harvest
+     * @param provider        OAI-PMH provider that this thread will harvest
      * @param actionSequences list of actions to take on harvested metadata
-     * @param cycle the harvesting cycle
-     *
-     * kj: ideally, give a worker the endpoint URI instead of the provider
+     * @param cycle           the harvesting cycle
+     *                        <p>
+     *                        kj: ideally, give a worker the endpoint URI instead of the provider
      */
     public Worker(Provider provider, List<ActionSequence> actionSequences,
                   Cycle cycle) {
 
-	this.provider = provider;
+        this.provider = provider;
 
-	this.actionSequences = actionSequences;
+        this.actionSequences = actionSequences;
 
         // register the endpoint with the cycle, kj: get the group
         endpoint = cycle.next(provider.getOaiUrl(), "group", provider.getScenario());
 
         // get the name of the scenario the worker needs to apply
         this.scenarioName = endpoint.getScenario();
+        total.incrementAndGet();
     }
 
     /**
      * <br>Start this worker thread <br><br>
-     *
+     * <p>
      * This method will block for as long as necessary until a thread can be
      * started without violating the limit.
      */
     public void startWorker() {
-	for (;;) {
-	    try {
-		semaphore.acquire();
-		break;
-	    } catch (InterruptedException e) { }
-	}
-	Thread t = new Thread(this);
-	t.start();
+        for (; ; ) {
+            try {
+                semaphore.acquire();
+                break;
+            } catch (InterruptedException e) {
+            }
+        }
+        Thread t = new Thread(this);
+        t.start();
     }
 
     @Override
@@ -133,7 +149,7 @@ class Worker implements Runnable {
             // factory for OAI verbs
             OAIFactory oaiFactory = new OAIFactory();
 
-            logger.info("Processing provider " + provider + " using " + scenarioName + " scenario and timeout " + provider.getTimeout() + " and retry ("+provider.getMaxRetryCount()+","+provider.getRetryDelays()+")");
+            logger.info("Processing provider " + provider + " using " + scenarioName + " scenario and timeout " + provider.getTimeout() + " and retry (" + provider.getMaxRetryCount() + "," + provider.getRetryDelays() + ")");
 
             FileSynchronization.addProviderStatistic(provider);
 
@@ -146,21 +162,21 @@ class Worker implements Runnable {
                 Scenario scenario = new Scenario(provider, actionSequence);
 
                 if (provider instanceof StaticProvider) {
-                    logger.debug("static harvest["+provider+"]");
+                    logger.debug("static harvest[" + provider + "]");
 
                     // set type of format harvesting to apply
                     AbstractHarvesting harvesting = new StaticPrefixHarvesting(
                             oaiFactory,
                             (StaticProvider) provider,
                             actionSequence);
-                    logger.debug("harvesting["+harvesting+"]");
+                    logger.debug("harvesting[" + harvesting + "]");
 
                     // get the prefixes
                     prefixes = scenario.getPrefixes(harvesting);
-                    logger.debug("prefixes["+prefixes+"]");
+                    logger.debug("prefixes[" + prefixes + "]");
 
                     if (prefixes.isEmpty()) {
-                        logger.debug("no prefixes["+prefixes+"] -> done");
+                        logger.debug("no prefixes[" + prefixes + "] -> done");
                         done = false;
                     } else {
                         // set type of record harvesting to apply
@@ -170,14 +186,14 @@ class Worker implements Runnable {
                         // get the records
                         if (scenarioName == CycleProperties.Scenario.ListIdentifiers) {
                             done = scenario.listIdentifiers(harvesting);
-                            logger.debug("list identifiers -> done["+done+"]");
+                            logger.debug("list identifiers -> done[" + done + "]");
                         } else {
                             done = scenario.listRecords(harvesting);
-                            logger.debug("list records -> done["+done+"]");
+                            logger.debug("list records -> done[" + done + "]");
                         }
                     }
                 } else {
-                    logger.debug("dynamic harvest["+provider+"]");
+                    logger.debug("dynamic harvest[" + provider + "]");
 
                     // set type of format harvesting to apply
                     AbstractHarvesting harvesting = new FormatHarvesting(oaiFactory,
@@ -185,11 +201,11 @@ class Worker implements Runnable {
 
                     // get the prefixes
                     prefixes = scenario.getPrefixes(harvesting);
-                    logger.debug("prefixes["+prefixes+"]");
+                    logger.debug("prefixes[" + prefixes + "]");
 
                     if (prefixes.isEmpty()) {
                         // no match
-                        logger.debug("no prefixes["+prefixes+"] -> done");
+                        logger.debug("no prefixes[" + prefixes + "] -> done");
                         done = false;
                     } else {
                         // determine the type of record harvesting to apply
@@ -200,7 +216,7 @@ class Worker implements Runnable {
 
                             // get the records
                             done = scenario.listIdentifiers(harvesting);
-                            logger.debug("list identifiers -> done["+done+"]");
+                            logger.debug("list identifiers -> done[" + done + "]");
                         } else {
                             harvesting = new RecordListHarvesting(oaiFactory,
                                     provider, prefixes, metadataFactory, endpoint);
@@ -209,7 +225,7 @@ class Worker implements Runnable {
                             done = scenario.listRecords(harvesting);
                             logger.debug("list records -> done[" + done + "]");
                         }
-                        if(Main.config.isIncremental() && endpoint.allowIncrementalHarvest()) {
+                        if (Main.config.isIncremental() && endpoint.allowIncrementalHarvest()) {
                             FileSynchronization.execute(provider);
                         }
                     }
@@ -226,22 +242,22 @@ class Worker implements Runnable {
             endpoint.setIncrement(FileSynchronization.getProviderStatistic(provider).getHarvestedRecords());
             logger.info("Processing finished for " + provider);
         } catch (Throwable e) {
-            logger.error("Processing failed for " + provider+": "+e.getMessage(),e);
+            logger.error("Processing failed for " + provider + ": " + e.getMessage(), e);
             t = e;
             throw e;
         } finally {
             provider.close();
-                
+
             ThreadContext.clearAll();
-            
+
             // tell the main log how it went
             if (t != null)
-                logger.error("Processing failed for " + provider+": "+t.getMessage(),t);
+                logger.error("Processing failed for " + provider + ": " + t.getMessage(), t);
             else
                 logger.info("Processing finished for " + provider);
 
             semaphore.release();
-
+            total.decrementAndGet();
             logger.debug("Goodbye from OAI Harvest Manager worker!");
         }
     }
